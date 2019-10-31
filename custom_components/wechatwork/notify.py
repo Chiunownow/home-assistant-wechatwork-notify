b6b0763645e81adce2253c1c83a1c5688d0376d6
chiunownow <chiunownow@gmail.com>
2019-11-01 03:53:45 +0800

    1. 修复发送图片时标题不能为空的bug 2. 现在推送图片，除了data-photo还允许使用data-image 3. 优化代码逻辑 4. 优化log输出

--- custom_components/wechatwork/notify.py
+++ custom_components/wechatwork/notify.py
@@ -1,15 +1,12 @@
 import logging
-#import time
-#import datetime
 import requests
 import json,os
 import voluptuous as vol
-#import sys
+import sys
 
 from homeassistant.components.notify import (
     ATTR_MESSAGE, ATTR_TITLE, ATTR_DATA, ATTR_TARGET, PLATFORM_SCHEMA, BaseNotificationService)
 import homeassistant.helpers.config_validation as cv
-#以上照抄
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,8 +15,8 @@ CONF_AGENTID = 'agentId'
 CONF_SECRET = 'secret'
 CONF_TOUSER = 'touser'
 CONF_HAURL = 'ha_url'
-#定义配置文件参数变量
 ATTR_PHOTO = 'photo'
+ATTR_IMAGE = 'image'
 ATTR_VIDEO = 'video'
 ATTR_FILE = 'file'
 
@@ -30,7 +27,6 @@ def get_service(hass, config, discovery_info=None):
     touser = config.get(CONF_TOUSER)
     ha_url = config.get(CONF_HAURL)
     return QiyeweichatNotificationService(hass, corpid, agentId, secret, touser, ha_url)
-#从配置文件获取参数
 
 class QiyeweichatNotificationService(BaseNotificationService):
 
@@ -40,7 +36,6 @@ class QiyeweichatNotificationService(BaseNotificationService):
         self.AGENTID = agentId
         self.TOUSER = touser
         self.HAURL = ha_url
-    #将参数赋予到变量
 
     def _get_access_token(self):
         url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
@@ -50,12 +45,10 @@ class QiyeweichatNotificationService(BaseNotificationService):
         req = requests.post(url, params=values)
         data = json.loads(req.text)
         return data["access_token"]
-    #获取token
 
     def get_access_token(self):
         access_token = self._get_access_token()
         return access_token
-    #将token赋值到变量
 
     def send_message(self, message='', **kwargs):
         send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + self.get_access_token()
@@ -69,51 +62,55 @@ class QiyeweichatNotificationService(BaseNotificationService):
             else:
                 msgtype = "text"
                 content = '{"content": "%s"}' % message
-        else:
+        else: #当data不为空时，上传文件并获取media_id
             if ATTR_VIDEO in data:
                 path = data.get(ATTR_VIDEO, None)
-                print(path)
-                curl = 'https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=' + self.get_access_token() + '&type=video'
-                files = {'video': open(path, 'rb')}
-                r = requests.post(curl, files=files)
-                print(r.text)
-                re = json.loads(r.text)
-                ree = re['media_id']
-                media_id = str(ree)
+                media_type = "video"
+                msgtype = "video"                
+            if ATTR_PHOTO in data:
+                path = data.get(ATTR_PHOTO, None)
+                media_type = "image"
+                msgtype = "mpnews"
+                if title is None:
+                    title = message
+            if ATTR_IMAGE in data:
+                path = data.get(ATTR_IMAGE, None)
+                media_type = "image"
+                msgtype = "mpnews"
+                if title is None:
+                    title = message
+            if ATTR_FILE in data:
+                path = data.get(ATTR_FILE, None)
+                media_type = "file"
+                msgtype = "file"
+            curl = 'https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=' + self.get_access_token() + '&type=' + media_type
+            files = {media_type: open(path, 'rb')}
+            _LOGGER.debug("Uploading media " + path + " to WeChat servicers")
+            r = requests.post(curl, files=files)
+            #任务超时未处理
+            _LOGGER.debug(r.text)
+            media_id = str(json.loads(r.text)['media_id'])
+            if int(json.loads(r.text)['errcode']) == 0:
+                _LOGGER.debug("Upload completed! media_id is "+media_id)
+            else:
+                _LOGGER.error("Upload failed. Error Code " + str(json.loads(r.text)['errcode']) + ". " + str(json.loads(r.text)['errmsg']))
+            if media_type == "video":
                 if title is None:
                     content = '{"media_id":"%s", "description":"%s"}' % (media_id, message)
                 else:
                     content = '{"media_id":"%s", "title":"%s", "description":"%s"}' % (media_id, title, message)
-                msgtype = "video"
-            if ATTR_PHOTO in data and title is not None:
-                path = data.get(ATTR_PHOTO, None)
-                print(path)
-                curl = 'https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=' + self.get_access_token() + '&type=video'
-                files = {'image': open(path, 'rb')}
-                r = requests.post(curl, files=files)
-                print(r.text)
-                re = json.loads(r.text)
-                ree = re['media_id']
-                media_id = str(ree)
+            if media_type == "image":
+                if title is None:
+                    title = message
                 content = '{"articles":[{"title":"%s","thumb_media_id":"%s","digest":"%s"}]}' % (title, media_id, message)
-                msgtype = "mpnews"
-            if ATTR_PHOTO in data and title is None:
-                _LOGGER.error("使用企业微信发送图片时必须有title")
-            if ATTR_FILE in data:
-                path = data.get(ATTR_FILE, None)
-                print(path)
-                curl = 'https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=' + self.get_access_token() + '&type=video'
-                files = {'file': open(path, 'rb')}
-                r = requests.post(curl, files=files)
-                print(r.text)
-                re = json.loads(r.text)
-                ree = re['media_id']
-                media_id = str(ree)
+            if media_type == "file":
                 send_file = '{"touser":"%s","msgtype":"file","agentid":%s,"file":{"media_id":"%s"}}' % (self.TOUSER,self.AGENTID,media_id)
                 send_file8 = send_file.encode('utf-8')
                 response = requests.post(send_url,send_file8)
-                print(send_file8)
-                print(response.text)
+                if int(json.loads(response.text)['errcode']) == 0:
+                    _LOGGER.debug("File sent successfully")
+                else:
+                    _LOGGER.error("File failed to send. Error code " + str(json.loads(response.text)['errcode']) + ". " + str(json.loads(response.text)['errmsg']))
                 if title:
                     msgtype = "textcard"
                     content = '{"title": "%s", "description": "%s", "url": "%s"}' % (title, message, self.HAURL)
@@ -123,5 +120,7 @@ class QiyeweichatNotificationService(BaseNotificationService):
         send_data = '{"touser": "%s","msgtype": "%s","agentid": %s,"%s":%s}' % (self.TOUSER,msgtype,self.AGENTID,msgtype,content)
         send_data8 = send_data.encode('utf-8')
         response = requests.post(send_url,send_data8)
-        print(send_data)
-        print(response.text)
+        if int(json.loads(response.text)['errcode']) == 0:
+            _LOGGER.debug("Message sent successfully")
+        else:
+            _LOGGER.error("Message failed to send. Error code " + str(json.loads(response.text)['errcode']) + ". " + str(json.loads(response.text)['errmsg']))
